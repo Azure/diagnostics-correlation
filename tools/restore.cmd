2788bca0cbf7f5843c69280ed277b0bd34e4fe52
Liudmila Molkova <lmolkova@microsoft.com>
2016-11-03 18:35:06 -0700

    Fix restore sources

--- tools/restore.cmd
+++ tools/restore.cmd
@@ -1,5 +1,5 @@
 @ECHO OFF
-dotnet restore %~dp0..\ --source "https://api.nuget.org/v3/index.json" --source "\\ddfiles\Team\Public\Correlation\Nugets\signed"
+dotnet restore %~dp0..\
 pushd %~dp0..
 tools\nuget.exe restore "src\Microsoft.Diagnostics.Correlation.Signing\Microsoft.Diagnostics.Correlation.Signing.csproj" -PackagesDirectory packages
 popd
